6cfce3749f53625b376eadabe09c0441960c1574
ricar <1821954281@qq.com>
2022-09-20 10:33:54 +0800

    first commit

---
+++ demo-neo4j/src/main/java/com/hecf/Neo4JApplication.java
@@ -0,0 +1,16 @@
+package com.hecf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author HeYuan
+ * @Date 2022/9/19 10:45
+ */
+@SpringBootApplication
+public class Neo4JApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4JApplication.class, args);
+    }
+}
