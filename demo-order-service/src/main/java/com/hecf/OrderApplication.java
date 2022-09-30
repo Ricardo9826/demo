e20072b05c50a5de0017d736f5df5657709011cf
ricar <1821954281@qq.com>
2022-09-30 16:16:18 +0800

    修改依赖

---
+++ demo-order-service/src/main/java/com/hecf/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.hecf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author HeYuan
+ * @Date 2022/9/22 15:14
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
